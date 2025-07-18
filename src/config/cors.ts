d2aedf392771c7436f0a3946b753f46c23a88967
Barbara <95110916+BarbaraMartel@users.noreply.github.com>
2025-07-17 22:04:47 -0500

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,22 @@
 import { CorsOptions } from 'cors'
 
-export const corsConfig : CorsOptions = {
-    origin: function(origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL]
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whiteList = [
+      'http://localhost:5173',
+      'https://devtree-barbaramartel.netlify.app'
+    ]
 
-        if(process.argv[2] === '--api') {
-            whiteList.push(undefined)
-        }
-
-        if(whiteList.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+    if (process.argv[2] === '--api') {
+      whiteList.push(undefined) // permite llamadas sin Origin (por Postman o Thunder)
     }
+
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
